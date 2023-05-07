45fbbcee540bf97599b05a11f0560d4eb3b25c55
Artem Mazin <minima1s@yandex.ru>
2023-05-07 22:37:20 +0300

    feat: добавить стили и компоненты для header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,24 @@ import logo from '../images/logo.svg';
 
 function Header() {
   return (
-    <header className='header wrapper'>
+    <header className="header wrapper">
       <img
         src={logo}
-        alt='Логотип проекта: Место'
-        className='header__logo'
+        alt="Логотип проекта: Место"
+        className="header__logo"
       />
+      <div className="header__profile">
+        <a
+          href="#"
+          className="header__email">
+          email@mail.com
+        </a>
+        <button
+          type="button"
+          className="header__button">
+          Выйти
+        </button>
+      </div>
     </header>
   );
 }
