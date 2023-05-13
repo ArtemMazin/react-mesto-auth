a21f789b5962ba9c6f7b03a395d40e38d8473365
Artem Mazin <minima1s@yandex.ru>
2023-05-13 16:12:28 +0300

    feat: сделать бургер-меню и скрытое меню профиля

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,22 @@
+import { useEffect, useState } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import logo from '../images/logo.svg';
 
 function Header({ loggedIn, setLoggedIn, email }) {
   const location = useLocation();
   const navigate = useNavigate();
+
+  const [isMenu, setIsMenu] = useState(false);
+
+  useEffect(() => {
+    //при входе/выходе меню должно быть изначально скрыто
+    setIsMenu(false);
+  }, [loggedIn]);
+
+  function toggleMenu(params) {
+    setIsMenu(!isMenu);
+  }
+
   function signOut() {
     localStorage.removeItem('token');
     setLoggedIn(false);
@@ -11,30 +24,49 @@ function Header({ loggedIn, setLoggedIn, email }) {
   }
 
   return (
-    <header className="header wrapper">
-      <img
-        src={logo}
-        alt="Логотип проекта: Место"
-        className="header__logo"
-      />
-      {loggedIn ? (
-        <div className="header__profile">
-          <p className="header__email">{email}</p>
-          <button
-            type="button"
-            className="header__button"
-            onClick={signOut}>
-            Выйти
-          </button>
-        </div>
-      ) : (
-        <Link
-          //location.pathname показывает текущее местоположение
-          to={location.pathname === '/sign-up' ? '/sign-in' : '/sign-up'}
-          className="header__link">
-          {location.pathname === '/sign-up' ? 'Войти' : 'Регистрация'}
-        </Link>
-      )}
+    <header className="header">
+      <div className={`header__profile-hide  ${isMenu ? 'header__profile-hide_active' : ''}`}>
+        <p className="header__email">{email || ''}</p>
+        <button
+          type="button"
+          className="header__button"
+          onClick={signOut}>
+          Выйти
+        </button>
+      </div>
+      <div className="header__content">
+        <img
+          src={logo}
+          alt="Логотип проекта: Место"
+          className="header__logo"
+        />
+        {loggedIn ? (
+          <>
+            <div
+              className="header__menu-hamburger"
+              onClick={toggleMenu}>
+              <span className="header__menu-bar"></span>
+            </div>
+
+            <div className="header__profile header__profile_mobile">
+              <p className="header__email">{email}</p>
+              <button
+                type="button"
+                className="header__button"
+                onClick={signOut}>
+                Выйти
+              </button>
+            </div>
+          </>
+        ) : (
+          <Link
+            //location.pathname показывает текущее местоположение
+            to={location.pathname === '/sign-up' ? '/sign-in' : '/sign-up'}
+            className="header__link">
+            {location.pathname === '/sign-up' ? 'Войти' : 'Регистрация'}
+          </Link>
+        )}
+      </div>
     </header>
   );
 }
